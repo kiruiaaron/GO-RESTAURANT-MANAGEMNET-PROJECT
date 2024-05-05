87b4494ef2c731b53885df5ee6044e2c9d6a20f9
kiruiaaron <kiruiaaron471@gmail.com>
2024-05-05 22:27:02 +0300

    new updates

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -17,7 +17,7 @@ import (
 )
 
 var orderCollection *mongo.Collection = database.OpenCollection(database.Client, "order")
-
+var tableCollection *mongo.Collection = database.OpenCollection(database.Client, "table")
 
 func GetOrders() *gin.HandlerFunc{
 	return func (c *gin.Context)  {
@@ -53,9 +53,50 @@ func GetOrder() *gin.HandlerFunc{
 }
 func CreateOrder() *gin.HandlerFunc{
 	return func (c *gin.Context)  {
-		
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var table models.Table
+		var order models.Order
+
+		if err := c.BindJSON(&oder); err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+			return
+		}
+
+		validationErr := validate.Struct(order)
+
+		if validationErr != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error":validationErr.Error()})
+			return
+		}
+
+		if order.Table_id != nil{
+			err := tableCollection.FindOne(ctx, bson.M{"table_id":order.table_id}).Decode(&table)
+			defer cancel()
+			if err != nil{
+				msg := fmt.Sprintf("message: Table was not found")
+				c.JSON(http.StatusInternalServerError, gin.H{"error":msg})
+				return
+			}
+
+		}
+		order.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		order.Updated_at,_=time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		order.ID = primitive.NewObjectID()
+		order.Order_id = order.ID.Hex()
+
+		result, insertErr := orderCollection.InsertOne(ctx, order)
+		if insertErr != nil{
+			msg:= fmt.Sprintf("order item was not created")
+			c.JSON(http.StatusInternalServerError, gin.H{"error":msg})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusOK,result)
 	}
 }
+
+
+
 func UpdateOrder() *gin.HandlerFunc{
 	return func (c *gin.Context)  {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
@@ -107,4 +148,18 @@ func UpdateOrder() *gin.HandlerFunc{
 		c.JSON(http.StatusOK, result)
 		}
 	}
+}
+
+
+func orderItemCretor(order models.Order) string{ 
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	order.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	order.Updated_at,_=time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	order.ID = primitive.NewObjectID()
+	order.Order_id = order.ID.Hex()
+
+	orderCollection.InsertOne(ctx, order)
+	defer cancel()
+
+	return order.Order_id
 }
